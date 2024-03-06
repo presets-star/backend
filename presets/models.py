689edf121ba1eb3871f69653c45724dd69277238
Kamil Ayupov <ayupovkamil@gmail.com>
2024-03-06 19:25:35 +0300

    Add file_path field to Cover

--- presets/models.py
+++ presets/models.py
@@ -7,7 +7,7 @@ class Preset(models.Model):
     name = models.CharField(max_length=50)
     scope = models.BooleanField()
     file = models.FileField()
-    file_type = models.Choices((('mp3', 'mp3'), ('wav', 'wav')))
+    file_type = models.Choices((("mp3", "mp3"), ("wav", "wav")))
     software = models.Choices((("FL", "FrutieLoops"), ("AB", "Ableton")))
     seller_id = models.ForeignKey("User", on_delete=models.CASCADE)
     rating = models.PositiveIntegerField(default=0)
@@ -34,4 +34,6 @@ class Review(models.Model):
 class Cover(models.Model):
 
     preset_id = models.OneToOneField("Preset", on_delete=models.CASCADE)
-    file_url = models.URLField(default="default directory")  # Add default directory
+    file_path = models.FilePathField(
+        default="defualt file path"
+    )  # Add default file path
