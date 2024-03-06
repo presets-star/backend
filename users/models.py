d6e886291a7e32a75f67e24e682c6f5736e69ecd
Kamil Ayupov <ayupovkamil@gmail.com>
2024-03-06 19:58:22 +0300

    Make Profile prettier

--- users/models.py
+++ users/models.py
@@ -4,24 +4,31 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 User = get_user_model()
-genders = ("male", "female", "helicopter")
 
 
 class Profile(models.Model):
+    genders = ("male", "female", "helicopter")
+
+    date_birth = models.DateField(blank=True, null=True)
 
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_birth = models.DateField(blank=True, null=True)
-    rating = models.IntegerField(blank=True, null=True)
     favourites = models.ManyToManyField("Preset", blank=True)
+
     buys = models.IntegerField(blank=True, null=True)
     sold = models.IntegerField(blank=True, null=True)
+    rating = models.IntegerField(blank=True, null=True)
+
     subscribers = models.PositiveIntegerField(default=0)
     tracks = models.PositiveIntegerField(default=0)
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+
     email = models.EmailField()
-    phone = PhoneNumberField(max_length=20)
+
     gender = models.Choices(genders)
+
+    phone = PhoneNumberField(max_length=20)
 
     class Meta:
         verbose_name = "Profile"
