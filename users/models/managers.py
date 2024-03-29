ae6d1a1ae73b9f46d2986a1988652486bd6785cd
Kamil Ayupov <ayupovkamil@gmail.com>
2024-03-29 22:19:00 +0300

    Add custom user manager

--- users/models/managers.py
+++ users/models/managers.py
@@ -17,19 +17,19 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_user(
-            self, email=None, password=None, username=None, **extra_fields
+            self, email=None, password=None, **extra_fields
     ):
         extra_fields.setdefault("is_superuser", False)
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_active", True)
 
-        return self._create_user(email, password, username, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(
-        self, email=None, password=None, username=None, **extra_fields
+        self, email=None, password=None, **extra_fields
     ):
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_active", True)
 
-        return self._create_user(email, password, username, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
